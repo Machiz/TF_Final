7acbe5d6161d73ee62dfd2ae91c5183fa0e074ec
Machiz <103539848+Machiz@users.noreply.github.com>
2024-06-07 22:27:34 -0500

    implementacion robin hood algorithm

--- TB_G6/Hashtable.h
+++ TB_G6/Hashtable.h
@@ -18,5 +18,44 @@ public:
 		}
 		numElementos = 0;
 	}
+	~Hashtable()
+	{
+		for (int i = 0; i < TABLE_SIZE; i++)
+		{
+			if (tabla[i] != nullptr)
+			{
+				delete tabla[i];
+			}
+		}
+		delete[] tabla;
+	}
+	int size()
+	{
+		return TABLE_SIZE;
+	}
+	int sizeActual()
+	{
+		return numElementos;
+	}
+	int buscar(int key)
+	{
+		int step = 0;
+		int i, base;
+		i = base = key % TABLE_SIZE;//obtener el indice
+		while (1)
+		{
+			if (tabla[i] == nullptr)
+			{
+				return -1;
+			}
+			else if (tabla[i]->getkey() == key)return i;//compara el valor de getkey con el de la tabla
+			else step++;
+			i = (base + step) % TABLE_SIZE;
+		}
+	}
+	void robinHood(int key, int value)
+	{
+
+	}
 };
 
